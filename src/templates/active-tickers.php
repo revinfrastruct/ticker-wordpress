d747b1c5cba3dd5003701c219115fac9576b817d
Erik Eklund <erik@valerius.se>
2016-10-17 12:50:24 +0200

    Added localstorage capabilities for active events and auto-selecting category for new ticks

--- src/templates/active-tickers.php
+++ src/templates/active-tickers.php
@@ -1,13 +1,18 @@
 <div class="wrap">
 <?php if (!empty($tickers)) : ?>
-<h3>Currently active ticker
+<div id="message" class="updated notice">
+<p><strong>Currently active ticker event:</strong>
 <select name="active_ticker">
+    <option value="null" selected>
+        --No active event--
+    </option>
 <?php foreach ($tickers as $ticker) : ?>
     <option value="<?php echo $ticker->term_id; ?>">
         <?php echo $ticker->name; ?>
     </option>
 <?php endforeach; ?>
 </select>
-</h3>
+</p>
+</div>
 <?php endif; ?>
 </div>
